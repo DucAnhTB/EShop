09ce56e5de9d7c92b3468459cf5392d302faf734
Đức Anh Bùi <chauongchan@gmail.com>
2021-06-07 03:48:47 +0700

    add-migration

---
+++ eShop.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.Configurations
+{
+    public class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.HasKey(x => new { x.CategoryId, x.ProductId });
+            builder.ToTable("ProductInCategories");
+            builder.HasOne(p => p.Product).WithMany(pc => pc.ProductInCategories).HasForeignKey(p => p.ProductId);
+            builder.HasOne(c => c.Category).WithMany(pc => pc.ProductInCategories).HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
